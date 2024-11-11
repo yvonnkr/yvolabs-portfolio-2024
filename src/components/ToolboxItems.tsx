3bf5610256d74cb70d0e5ddaa7519af709a10f93
yvonnkr <yvonnkr86@gmail.com>
2024-11-11 22:04:27 +0000

    implement about section

---
+++ src/components/ToolboxItems.tsx
@@ -0,0 +1,33 @@
+import {TechIcon} from "@/components/TechIcon";
+import {twMerge} from "tailwind-merge";
+import React from "react";
+
+
+export const ToolboxItems = (
+    {
+        items,
+        className,
+        itemsWrapperClassName,
+    }: {
+        items: { title: string, iconType: React.ElementType }[],
+        className?: string,
+        itemsWrapperClassName?: string,
+    }) => {
+    return (
+        <div
+            className={twMerge("flex  [mask-image:linear-gradient(to_right,transparent,black_10%,black_70%,transparent)]", className)}
+        >
+            <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6 ", itemsWrapperClassName)}>
+                {items.map((item) => (
+                    <div key={item.title}
+                         className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+                    >
+                        <TechIcon component={item.iconType}/>
+                        <span className="font-semibold">{item.title}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+
+}
